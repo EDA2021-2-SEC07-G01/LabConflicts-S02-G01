c35f1d122ce15aaa3db77e3f0ee82c17c115181f
naparicioc <n.aparicioc@uniandes.edu.co>
2021-08-24 22:39:02 -0500

    cambios del Estudiante 1

--- App/view.py
+++ App/view.py
@@ -42,7 +42,8 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Modificación de Est-1 en el Lab 2
+    #Modificación de Est-1 en el Lab 2
+    print("3- Cargar Book-Tags!")
     print("0- Salir")
 
 
@@ -64,8 +65,8 @@ def loadBookTags():
     """
     Cargar los Tags de libros
     """
-    # TODO: Modificación de Est-1 en el Lab 2
-    pass
+    #Modificación de Est-1 en el Lab 2
+    return controller.loadBooksTags('GoodReads/book_tags-small.csv')
 
 
 """
@@ -79,7 +80,8 @@ while True:
         books = loadBooks()
         print('Total de libros cargados: ' + str(lt.size(books)))
 
-        # TODO: Modificación de Est-1 en el Lab 2
+        #Modificación de Est-1 en el Lab 2
+        print('Primer libro cargado: ' + str(lt.firstElement(books)))
 
 
         # TODO: Modificación de Est-2 en el Lab 2
